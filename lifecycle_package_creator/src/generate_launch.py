21f4560afcd8c75ac36b3ceff67e8ac03f8fcdc5
Myron Rodrigues <rodriguesmyron47@gmail.com>
2024-06-23 11:23:21 +0000

    added launch template

---
+++ lifecycle_package_creator/src/generate_launch.py
@@ -0,0 +1,23 @@
+from jinja2 import Environment, FileSystemLoader
+
+from utils import *
+
+
+def generate_node_launch(template_dir: str, launch_template: str,
+                         node_name: str, namespace: str):
+    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True)
+
+    env.filters['snake_case'] = to_snake_case
+
+    launch_template = env.get_template(launch_template)
+    launch_file = launch_template.render(
+        node_name=node_name, namespace=namespace)
+    print(launch_file)
+
+
+if __name__ == "__main__":
+    template_dir = "/home/myron/athena/ros2_tools/lifecycle_package_creator/templates"
+    launch_template = "node_launch.template"
+
+    generate_node_launch(template_dir, launch_template, "CodeMaker",
+                         "codega")
