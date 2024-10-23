92ba50bb0e5007568ca9a08feef930fd2d9149dd
Myron Rodrigues <rodriguesmyron47@gmail.com>
2024-10-23 23:49:23 +0000

    use generator for images

--- euroc_rosbag2/image_handler.py
+++ euroc_rosbag2/image_handler.py
@@ -1,13 +1,13 @@
 import rosbag2_py
-from rclpy.serialization import serialize_message
-import numpy as np
 from cv_bridge import CvBridge
-import builtin_interfaces.msg
 from std_msgs.msg import Header
+from utils import to_ros_timestamp
+from pathlib import Path
+import cv2
 
 
 def create_topic(
-    writer,
+    writer: rosbag2_py.SequentialWriter,
     id: int,
     topic: str,
     topic_type="sensor_msgs/msg/Image",
@@ -23,18 +23,20 @@ def create_topic(
     )
 
 
-def write_message(
-    writer: rosbag2_py.SequentialCompressionWriter,
-    frame_id: str,
-    topic: str,
-    image: np.array,
-    timestamp: int,
+def msg_generator(
+    cam_dir: Path, frame_id: str, topic: str, img_suffixes=(".png", ".jpg")
 ):
-    header = Header()
-    header.frame_id = frame_id
-    ts_s = int(timestamp // 1e9)
-    ts_ns = int(timestamp % 1e9)
-    header.stamp = builtin_interfaces.msg.Time(sec=ts_s, nanosec=ts_ns)
-    msg = CvBridge().cv2_to_imgmsg(image, header=header, encoding="passthrough")
-    msg.height, msg.width = image.shape
-    writer.write(topic, serialize_message(msg), timestamp)
+    for img_path in cam_dir.iterdir():
+        if img_path.suffix in img_suffixes:
+            header = Header()
+            header.frame_id = frame_id
+            timestamp = int(img_path.stem)
+            header.stamp = to_ros_timestamp(timestamp)
+            img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
+            msg = CvBridge().cv2_to_imgmsg(img, header=header, encoding="passthrough")
+            msg.height, msg.width = img.shape
+            yield topic, msg, timestamp
+        else:
+            raise ValueError(
+                f"Image found with different suffix than ones in {img_suffixes}"
+            )
