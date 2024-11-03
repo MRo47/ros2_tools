2fb1a03a985cacfe712144ee799284e23cbccc72
Myron Rodrigues <rodriguesmyron47@gmail.com>
2024-11-03 22:10:49 +0000

    add tf static pub for bag

--- euroc_rosbag2/export.py
+++ euroc_rosbag2/export.py
@@ -57,34 +57,32 @@ def write_to(in_path: Path, output_path: Path):
     gt_h.create_twist_topic(writer, 7, data_maps["gt_vel"]["topic"])
     gt_h.create_imu_bias_topic(writer, 8, data_maps["gt_imu_bias"]["topic"])
 
-    # writer.create_topic(
-    #     rosbag2_py.TopicMetadata(
-    #         id=9,
-    #         name="/tf_static",
-    #         type="tf2_msgs/msg/TFMessage",
-    #         serialization_format="cdr",
-    #     )
-    # )
+    writer.create_topic(
+        rosbag2_py.TopicMetadata(
+            id=9,
+            name="/tf_static",
+            type="tf2_msgs/msg/TFMessage",
+            serialization_format="cdr",
+        )
+    )
 
-    # static_tf = TFMessage()
+    static_tf = TFMessage()
 
-    # static_tf.transforms = [
-    #     get_frame_transform_msg(
-    #         imu0_config_file, "base_link", data_maps["imu0"]["frame_id"]
-    #     ),
-    #     get_frame_transform_msg(
-    #         cam0_config_file, "base_link", data_maps["cam0_image"]["frame_id"]
-    #     ),
-    #     get_frame_transform_msg(
-    #         cam1_config_file, "base_link", data_maps["cam1_image"]["frame_id"]
-    #     ),
-    #     get_frame_transform_msg(
-    #         pos_config_file, "base_link", data_maps["leica0"]["frame_id"]
-    #     ),
-    #     get_frame_transform_msg(gt_config_file, "base_link", gt_frame),
-    # ]
-
-    # writer.write("/tf_static", serialize_message(static_tf), 0)
+    static_tf.transforms = [
+        get_frame_transform_msg(
+            imu0_config_file, "base_link", data_maps["imu0"]["frame_id"]
+        ),
+        get_frame_transform_msg(
+            cam0_config_file, "base_link", data_maps["cam0_image"]["frame_id"]
+        ),
+        get_frame_transform_msg(
+            cam1_config_file, "base_link", data_maps["cam1_image"]["frame_id"]
+        ),
+        get_frame_transform_msg(
+            pos_config_file, "base_link", data_maps["leica0"]["frame_id"]
+        ),
+        get_frame_transform_msg(gt_config_file, "map", "base_link"),
+    ]
 
     data_generators = [
         cam_h.msg_generator(
@@ -118,10 +116,15 @@ def write_to(in_path: Path, output_path: Path):
         ),
     ]
 
+    tf_published = False
+
     print("Writing data...")
     for i, gen in enumerate(data_generators):
         print(f"Writing data from generator {i}...")
         for topic, msg, timestamp in gen:
+            if not tf_published:
+                writer.write("/tf_static", serialize_message(static_tf), timestamp)
+                tf_published = True
             writer.write(topic, serialize_message(msg), timestamp)
 
     del writer
